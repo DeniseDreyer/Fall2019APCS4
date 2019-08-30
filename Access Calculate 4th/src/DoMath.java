7d960a7baf993f84bc47237260f356cf97ab8501
DeniseDreyer <ddreyer@millshighsch.org>
2019-08-30 09:51:03 -0700

    Part 0 mostly done

---
+++ Access Calculate 4th/src/DoMath.java
@@ -0,0 +1,14 @@
+/* Denise Dreyer
+ * Period 4
+ * August 29,2019
+ * Calls various math functions from the Calculate library
+ * Client or runner class
+ */
+public class DoMath {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ Calculate Library 4th/src/Calculate.java
@@ -0,0 +1,10 @@
+/* Denise Dreyer
+ * Period 4
+ * Library of Math functions
+ * August 29, 2019
+ */
+public class Calculate {
+	public static int square(int number) {
+		return number * number;
+	}
+}
