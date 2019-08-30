f68f001b23554f519ab633fd9552cb34d6bfcfef
DeniseDreyer <ddreyer@millshighsch.org>
2019-08-30 11:27:19 -0700

    part 1 thru average (not done yet)

--- Access Calculate 4th/src/DoMath.java
+++ Access Calculate 4th/src/DoMath.java
@@ -7,8 +7,8 @@
 public class DoMath {
 
 	public static void main(String[] args) {
-		
-
+		System.out.println(Calculate.square(-11));
+		System.out.println(Calculate.cube(-3));
 	}
 
 }
--- Calculate Library 4th/src/Calculate.java
+++ Calculate Library 4th/src/Calculate.java
@@ -4,7 +4,26 @@
  * August 29, 2019
  */
 public class Calculate {
+	//A call to square returns the square of the value passed.
+	//It accepts and returns an integer.
 	public static int square(int number) {
-		return number * number;
+		int answer=0;
+		answer = number*number;
+		return answer;
 	}
+	//A call to cube returns the cube of the value passed.
+	//It accepts and returns an integer.
+	public static int cube(int number) {
+		int answer=0;
+		answer = number*number*number;
+		return answer;
+	}
+	//A call to average returns the average of the values passed to it.
+	//It accepts 2 doubles and returns an double.
+	public static double average(double number1, double number2) {
+		double answer=0;
+		answer = (number1+number2)/2;
+		return answer;
+	}
+
 }
